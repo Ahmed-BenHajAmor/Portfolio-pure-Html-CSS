152e6fb215fc0d0ee6559ac1a37cba301604ba57
Ahmed BenHajAmor <ahmed.benhajamorr@gmail.com>
2024-09-09 16:08:49 +0000

    sliding skills

--- index.js
+++ index.js
@@ -6,12 +6,33 @@ const homeList = (text)=>{
     homeUl.appendChild(tempDiv.firstChild) 
 }
 
+const rollList = (text)=>{
+    const rollUl = document.querySelectorAll('.slide-div');
+    const li = `<h3 class="flex items-center">${text}</h3> <img class="h-full" src="assets/yellow-star.png" alt="star" />`;
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = li;
+
+    rollUl.forEach(el => {
+        const firstChildClone = tempDiv.firstChild.cloneNode(true); // Clone the first child
+        const lastChildClone = tempDiv.lastChild.cloneNode(true);   // Clone the last child
+
+        el.appendChild(firstChildClone);
+        el.appendChild(lastChildClone);
+    });
+
+    
+}
 const homeUlArr = ['Designer', 'Figma', 'Developer']
+const rollUlArr = ['UI/UX', 'DEVELOPMENT', 'PRODUCT DESIGN', 'SOCIAL MEDIA DESIGN', 'BRANDING']
 
 document.addEventListener('DOMContentLoaded', ()=>{
     homeUlArr.forEach(el => {
-        console.log('in');
         
         homeList(el)
+    })
+
+    rollUlArr.forEach(el => {
+        
+        rollList(el)
     })
 })
