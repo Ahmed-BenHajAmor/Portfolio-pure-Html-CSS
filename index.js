0076e5bb8b0ab5c8de1ef9f6756f001864a0998b
Ahmed BenHajAmor <ahmed.benhajamorr@gmail.com>
2024-09-09 13:18:52 +0000

    home-section-added

---
+++ index.js
@@ -0,0 +1,17 @@
+const homeList = (text)=>{
+    const homeUl = document.querySelector('#home-ul');
+    const li = `<li class="flex items-center"><img class="h-full" src="assets/yellow-start.png" alt=""><h2 class="font-medium text-4xl">${text}</h2></li>`
+    const tempDiv = document.createElement('div')
+    tempDiv.innerHTML = li
+    homeUl.appendChild(tempDiv.firstChild) 
+}
+
+const homeUlArr = ['Designer', 'Figma', 'Developer']
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    homeUlArr.forEach(el => {
+        console.log('in');
+        
+        homeList(el)
+    })
+})
